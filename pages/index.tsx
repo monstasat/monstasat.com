9bb8ece9b6e7bf5ad3833791b4abc9d970e52bf6
monstasat <sashayanin@gmail.com>
2020-05-16 23:53:46 +0300

    feat: 🎸 initialize project with nextjs, eslint, stylelint

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+
+import './global.css';
+
+const Home: React.FC = () => (
+  <div>
+    <Head>
+      <title>monstasat</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    home
+  </div>
+);
+
+export default Home;
