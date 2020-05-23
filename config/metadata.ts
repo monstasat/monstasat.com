c976453aa528f039ed75e2bdcff348913df3419e
monstasat <sashayanin@gmail.com>
2020-05-23 18:34:55 +0300

    feat: 🎸 add SEO component
    contains site metadata

---
+++ config/metadata.ts
@@ -0,0 +1,10 @@
+export default {
+  author: {
+    name: 'Alexander Yanin',
+    nickname: 'monstasat',
+  },
+  title: 'Alexander Yanin - enthusiastic frontend developer',
+  description: 'Alexander Yanin is an enthusiastic frontend developer '
+  + 'with some hardware background. Addicted to problem solving and tea drinking',
+  url: 'https://monstasat.com',
+};
